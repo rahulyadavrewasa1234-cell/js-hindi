5ff1b71b515d207b641454fed5c60dd2f4a6bc2f
rahulyadavrewasa1234-cell <rahulyadavrewasa1234@gmail.com>
2026-03-20 11:51:37 +0000

    conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,28 @@
 let score = 33
 
 console.log(typeof(score));
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ****************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "rahool"
+
+let str3 = str1 + str2
+console.log(str3);
