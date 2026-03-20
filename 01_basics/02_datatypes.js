380de27ebb616f6276ef8ad1623e3f6f10fc1a0b
rahulyadavrewasa1234-cell <rahulyadavrewasa1234@gmail.com>
2026-03-20 10:32:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //  we are using nodejs, not browser
+
+// readability is 1st priorty
+
+console.log(3 + 3)
+
+
+let name  = "rahool"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+// object
+
+console.log(typeof "rahool");    //string
+console.log(typeof age);         //number
+console.log(typeof null);     // object
+console.log(typeof undefined);  // undefined
+
